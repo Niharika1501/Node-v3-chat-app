37225d810d919ce5a01499cb8d4121aa7fee4b88
niharika <niharikamadke@gmail.com>
2023-01-27 17:37:17 +0530

    init

---
+++ src/utilis/users.js
@@ -0,0 +1,88 @@
+const users = []
+
+//addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    //Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room===room && user.username===username
+    })
+
+    // Validate username
+    if(existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => {
+        return user.id ===id
+    })
+    if (index !== -1){
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id===id)
+}
+
+const getUsersInRoom = (room) => {
+    return users.filter((user) => user.room===room)
+}
+
+// addUser({
+//     id: 22,
+//     username: 'Niharika',
+//     room: 'abc'
+// })
+
+// addUser({
+//     id: 42,
+//     username: 'Mike',
+//     room: 'abc'
+// })
+
+// addUser({
+//     id: 32,
+//     username: 'Jane',
+//     room: 'def'
+// })
+
+// const user = getUser(421)
+
+// console.log(user)
+
+// const userList = getUsersInRoom('abc')
+// console.log(userList)
+
+// const removedUser = removeUser(22)
+
+// console.log(removedUser)
+// console.log(users)
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
